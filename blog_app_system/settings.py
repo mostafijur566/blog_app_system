8ce891aae7879a08a7a48ad1790be3751cb7894e
Mostafijur Rahman <66210185+mostafijur566@users.noreply.github.com>
2022-06-05 01:35:01 +0600

    Update settings.py

--- blog_app_system/settings.py
+++ blog_app_system/settings.py
@@ -131,6 +131,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
